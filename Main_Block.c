a6ae4b508e45766a8d7c4506224abaa77b650802
Himanshu Jain <himanshujain.0074@gmail.com>
2023-10-31 13:35:32 +0530

    Minor updates in Main_Block.c

--- Main_Block.c
+++ Main_Block.c
@@ -6,8 +6,45 @@
 #include "Registration.c"
 int main()
 {
+    int user_input;
    registration_driver();
    printf("What would you like to do?\n");
-   printf("1. Check Balance\n2. Deposit money\n3. Withdraw money\n");
+   printf("1. Check Balance\n2. Deposit\n3. Withdraw4. Create an FD\n5. Apply for Loan\n6. Apply for Insurance\n7. Exit");
+   printf("Your choice: "); re_enter:
+   scanf("%d", &user_input);
+
+   switch(user_input)
+   {
+    case 1:
+    break;
+
+    case 2:
+    break;
+
+    case 3:
+    break;
+
+    case 4:
+    fd_driver();
+    break;
+
+    case 5:
+    loan_driver();
+    break;
+
+    case 6:
+    insurance_driver();
+    break;
+
+    case 7:
+    printf("\nDo visit again..");
+    goto the_end;
+    break;
+
+    default:
+    printf("Invalid input!\nEnter a valid choice: ");
+    goto re_enter;
+   }
+the_end:
     return 0;
 }
